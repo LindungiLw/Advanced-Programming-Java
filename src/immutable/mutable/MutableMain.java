33a2dfa865a5c50840b01890e04bac11050741c2
RahmaLindungiLaowo <rahmalindungilaowo@gmail.com>
2025-10-19 09:02:19 +0700

    immutable

---
+++ src/immutable/mutable/MutableMain.java
@@ -0,0 +1,10 @@
+package immutable.mutable;
+
+public class MutableMain {
+    public static void main(String[] args) {
+        MutableObj obj = new MutableObj(10);
+        obj.add(20);
+
+        System.out.println("obj = " + obj.getValue());
+    }
+}
